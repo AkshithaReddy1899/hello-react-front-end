0d8fa5c56c286c61996ce0a0baf96c1fc55ab921
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-06-03 11:17:16 +0530

    Add routes, fetch data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
+import { Provider } from "react-redux";
+import store from './redux/configureStore';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Greeting from './Component/Greeting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        App Component
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
